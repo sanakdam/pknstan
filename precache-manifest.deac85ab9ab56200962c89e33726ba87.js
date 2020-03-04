36b58fe3eeb2f5573f76dfee6de6809fc6b9bb57
akdam augusta <san.akdam@gmail.com>
2020-03-04 10:31:56 +0700

    first commit

---
+++ precache-manifest.deac85ab9ab56200962c89e33726ba87.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79f423b2595735c83ec66335c32d7aae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ddf0d08ede53d730786",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "e38653d8d2812bb9a83e",
+    "url": "/static/css/main.12354f72.chunk.css"
+  },
+  {
+    "revision": "7ddf0d08ede53d730786",
+    "url": "/static/js/2.1b168950.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.1b168950.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e38653d8d2812bb9a83e",
+    "url": "/static/js/main.fe4bb2a8.chunk.js"
+  },
+  {
+    "revision": "d57280561ea3f6488342",
+    "url": "/static/js/runtime-main.4248670b.js"
+  },
+  {
+    "revision": "e7d08872578ea3c0928e0d25518fb6ca",
+    "url": "/static/media/arrow-left.e7d08872.svg"
+  },
+  {
+    "revision": "18ac438a9e7f995dea9fe6de3fafddc6",
+    "url": "/static/media/arrow-right.18ac438a.svg"
+  }
+]);
